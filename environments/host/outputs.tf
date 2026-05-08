ae4135b675d004ee245d94eb4743a4777aa17621
Francisco <fmorenoreina@gmail.com>
2026-05-08 12:53:08 +0200

    fix output

--- environments/host/outputs.tf
+++ environments/host/outputs.tf
@@ -2,19 +2,7 @@ output "vpc_self_link" {
   value = module.network.vpc_self_link
 }
 
-output "subnet_standard_self_link" {
-  value = moudle.network.subnet_self_link["subnet-gke-standard"]
-
+output "subnets_map" {
+  value = module.network.subnets_map
 }
 
-output "pods_range_name" {
-  value = "pods-standard"
-}
-
-output "services_range_name" {
-  value = "services-standard"
-}
-
-output "vpc_name" {
-  value = module.network.vpc_name
-}
