a52a4ec28916fe15eec612da6c668cc9ab7cdb47
Francisco <fmorenoreina@gmail.com>
2026-05-08 18:56:21 +0200

    nuevos roles3

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -25,7 +25,9 @@ module "sa_github" {
     "roles/compute.securityAdmin",
     "roles/storage.admin",
     "roles/serviceusage.serviceUsageAdmin",
-    "roles/iam.workloadIdentityUser"
+    "roles/iam.workloadIdentityUser",
+    "roles/storage.admin",
+    "roles/resourcemanager.projectIamAdmin"
   ]
 }
 
@@ -39,7 +41,8 @@ resource "google_project_iam_member" "github_sa_dev_permissions" {
     "roles/compute.admin",                  # Poder crear los nodos del clúster
     "roles/iam.serviceAccountUser",         # Poder usar las SAs de los nodos
     "roles/resourcemanager.projectIamAdmin",# Poder gestionar permisos en DEV
-    "roles/artifactregistry.admin"          # Poder subir imágenes Docker
+    "roles/artifactregistry.admin",
+    "roles/iam.serviceAccountAdmin"         # Poder subir imágenes Docker
   ])
 
   project = var.project_id # ID del proyecto DEV
@@ -49,6 +52,15 @@ resource "google_project_iam_member" "github_sa_dev_permissions" {
   depends_on = [module.sa_github]
 }
 
+
+# Permiso explícito sobre el bucket del estado
+resource "google_storage_bucket_iam_member" "state_iam" {
+  bucket = "bucket-zafa-host-zafa"
+  role   = "roles/storage.objectAdmin"
+  member = "serviceAccount:${module.sa_github.email}"
+
+  depends_on = [module.sa_github]
+}
 # =============================================================================
 # 3. WORKLOAD IDENTITY FEDERATION (WIF)
 # =============================================================================
@@ -80,3 +92,24 @@ resource "google_service_account_iam_member" "github_wif_binding" {
   role               = "roles/iam.workloadIdentityUser"
   member             = "principalSet://iam.googleapis.com/${google_iam_workload_identity_pool.github_pool.name}/attribute.repository/fjmorenor/kubernetes-pro"
 }
+
+# 1. Obtener los datos del proyecto DEV para sacar el ID numérico
+data "google_project" "dev_project" {
+  project_id = var.project_id # El ID de zafa-dev-zafa
+}
+
+# 2. Dar permiso al Agente de Servicio de GKE de DEV sobre el proyecto HOST
+resource "google_project_iam_member" "gke_agent_network_user" {
+  project = var.host_project_id # zafa-host-zafa
+  role    = "roles/compute.networkUser"
+  member  = "serviceAccount:service-${data.google_project.dev_project.number}@container-engine-robot.iam.gserviceaccount.com"
+}
+
+# 3. Dar permiso también a la Service Account de GitHub sobre la red del HOST
+# (Para que Terraform pueda "leer" la subred al planificar)
+resource "google_project_iam_member" "github_sa_network_viewer" {
+  project = var.host_project_id
+  role    = "roles/compute.networkUser"
+  member  = "serviceAccount:${module.sa_github.email}"
+}
+
