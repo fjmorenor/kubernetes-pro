3672a9244539e18b838b668208584cfc1af33cb9
Francisco <fmorenoreina@gmail.com>
2026-05-08 19:54:32 +0200

    nuevos roles7

--- modules/shared-vpc/main.tf
+++ modules/shared-vpc/main.tf
@@ -3,15 +3,15 @@
 # =============================================================================
 # Este recurso designa formalmente a un proyecto como "Anfitrión" de la red.
 # Es el paso previo obligatorio para poder compartir subredes con otros proyectos.
-resource "google_compute_shared_vpc_host_project" "host" {
+#resource "google_compute_shared_vpc_host_project" "host" {
   # Recibe del Main: var.host_project_id (ej: "zafa-host-123").
   # Conecta con: El API de Compute Engine para habilitar la capacidad de Shared VPC.
-  project = var.host_project_id
+ # project = var.host_project_id
   
   # QUE HACE: Activa la funcionalidad de "Hosting" en el proyecto. 
   # A partir de este momento, las redes de este proyecto pueden ser "vistas"
   # por los proyectos de servicio que tú autorices.
-}
+#}
 
 # =============================================================================
 # RECURSO 2: VÍNCULO DEL PROYECTO DE SERVICIO (DEV)
@@ -21,10 +21,10 @@ resource "google_compute_shared_vpc_host_project" "host" {
 resource "google_compute_shared_vpc_service_project" "dev" {
   # Conecta con: El recurso host_project creado justo arriba.
   # Esto garantiza que el proyecto host esté listo antes de intentar vincular el servicio.
-  host_project    = google_compute_shared_vpc_host_project.host.project
+  host_project    = "zafa-host-zafa"
   
   # Recibe del Main: var.service_project_id (ej: "zafa-dev-456").
-  service_project = var.service_project_id
+  service_project = "zafa-dev-zafa"
   
   # QUE HACE: Crea una relación de confianza administrativa. 
   # Permite que el proyecto 'Dev' utilice los recursos de red del proyecto 'Host'
